6b40aea0a5fba72a21c285bc7eed98806acda119
陳力綱 <vincentchen@chenligangde-macbook-pro.local>
2016-07-08 18:26:57 +0800

    Initial Commit

---
+++ Cardscanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cardscanner
+//
+//  Created by 陳力綱 on 2016/7/8.
+//  Copyright © 2016年 陳力綱. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
